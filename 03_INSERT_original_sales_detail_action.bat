0214942e1bbec57257943d14ae910c1f470ffdc7
sakai kohei <sakai.kohei@isid-ao.co.jp>
2021-06-17 14:41:38 +0900

    コードの変更

---
+++ 03_INSERT_original_sales_detail_action.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+
+SET cnf="C:\ProgramData\MySQL\MySQL Server 8.0\login.cnf"
+SET sql="E:\sql_file\INSERT_original_sales_detail.sql"
+SET log="E:\logs\sql_bat\general\log.txt"
+SET error="E:\logs\sql_bat\error\error.txt"
+
+CHCP 65001
+mysql --defaults-extra-file=%cnf% <%sql% >>%error% 2>&1
+if %errorlevel%==1 ECHO %date% %time%>>%error%
+if %errorlevel%==1 ECHO %sql%の実行が失敗しました。>>%error%
+if %errorlevel%==0 ECHO %date% %time%>>%log%
+if %errorlevel%==0 ECHO %sql%が実行されました。>>%log%
