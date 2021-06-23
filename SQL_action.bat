4b5ba06515bdb1a8fbce9719d6be62654309ab6e
sakai kohei <sakai.kohei@isid-ao.co.jp>
2021-06-23 13:01:38 +0900

    新規作成

--- SQL_action.bat
+++ SQL_action.bat
@@ -1,10 +1,16 @@
-@echo off
+﻿@echo off
 CHCP 65001
 
 SET cnf="C:\ProgramData\MySQL\MySQL Server 8.0\login.cnf"
 SET /P paths="ファイルパスを入力してください。"
 SET result="E:\logs\sql_bat\result.txt"
 SET title=[SQL_Action]
+
+if not "%paths%"=="E:\bat_file\sql_action.txt" (
+echo %title% %date% %time% 入力されたパスは無効です。>>%result%& goto :eof
+) else ( 
+echo %title%SQLファイルの実行を始めます。>>%result%
+)
 
 setlocal enabledelayedexpansion
 for /f %%a in (%paths%) do (
