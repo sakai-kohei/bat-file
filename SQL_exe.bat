7a267bceface367632ad005013ed22c1b549c545
sakai kohei <sakai.kohei@isid-ao.co.jp>
2021-06-28 09:58:31 +0900

    コードの修正

--- SQL_exe.bat
+++ SQL_exe.bat
@@ -1,19 +1,32 @@
-﻿@echo off
+﻿@echo on
+
+CHCP 65001
 
 SET cnf="C:\ProgramData\MySQL\MySQL Server 8.0\login.cnf"
 SET paths=E:\\bat_file\\sql_exe.txt
-SET result="E:\logs\sql_bat\result.txt"
+SET result="E:\logs\bat\sql_exe\result.txt"
 SET title=[%date% %time% SQL_EXE]
 SET all=0
 SET count=1
 
+setlocal enabledelayedexpansion
 for /f %%b in (%paths%) do (
+
+SET txt=%%b
+echo !txt:~1,11! >>%result%
+if not "!txt:~0,12!"=="E:\sql_file\" (echo %title% 「sql_exe.txt」に記入したSQLファイルのパスが間違っています。バッチの実行を中止します。 >>%result%&goto :eof)
+echo !txt:~-3! >>%result%
+if not "!txt:~-3!"=="sql" (echo %title% 「sql_exe.txt」には、SQLファイルを記入してください。バッチの実行を中止します。 >>%result%&goto :eof)
 SET /a all+=1
+echo !all! >>%result%
 )
+
+if !all!==0 (echo %title% テキストに実行するSQLファイルが記載されていません。バッチの実行を中止します。 >>%result%&goto :eof)
+endlocal
 
 setlocal enabledelayedexpansion
 for /f %%a in (%paths%) do (
-echo %title% !count!/%all% %%aの実行を始めます。>>%result%
+echo %title% !count!/%all% %%aの実行を始めます。 >>%result%
 mysql --defaults-extra-file=%cnf% <%%a >>%result% 2>&1
 if !errorlevel! equ 1 (
 ECHO %title%  !count!/%all% %%aの実行が失敗しました。>>%result%&goto :eof  
