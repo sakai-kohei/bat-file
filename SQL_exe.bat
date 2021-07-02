85fdd0ad3db2bc720053df502739e762fa5e3721
sakai kohei <sakai.kohei@isid-ao.co.jp>
2021-07-02 19:11:44 +0900

    修正

--- SQL_exe.bat
+++ SQL_exe.bat
@@ -1,5 +1,6 @@
-﻿CHCP 65001
-@echo off
+﻿@echo off
+CHCP 65001
+
 REM =====================================================================
 REM  SQLファイル実行バッチ Ver1.0
 REM  Date  :2021/06/28
@@ -20,30 +21,36 @@ SET title=[%date% %time% SQL_EXE]
 SET all=0
 SET count=1
 
-setlocal enabledelayedexpansion
+
 for /f %%b in (%paths%) do (
-
 SET txt=%%b
-if not exist "!txt!" (echo %title% 「sql_exe.txt」に記入したSQLファイルのパスが間違っています。バッチの実行を中止します。 >>%result%&goto :eof)
-if not "!txt:~-3!"=="sql" (echo %title% 「sql_exe.txt」には、SQLファイルを記入してください。バッチの実行を中止します。 >>%result%&goto :eof)
+if exist "%txt%" (
+echo %title% 「sql_exe.txt」に記入したSQLファイルのパスが間違っています。バッチの実行を中止します。 
+echo %title% 「sql_exe.txt」に記入したSQLファイルのパスが間違っています。バッチの実行を中止します。 >>%result%&goto :eof
+)
+if not "%txt:~-3%"=="sql" (
+echo %title% 「sql_exe.txt」には、SQLファイルを記入してください。バッチの実行を中止します。
+echo %title% 「sql_exe.txt」には、SQLファイルを記入してください。バッチの実行を中止します。 >>%result%&goto :eof
+)
 SET /a all+=1
 )
-if !all!==0 (echo %title% テキストに実行するSQLファイルが記載されていません。バッチの実行を中止します。 >>%result%&goto :eof)
-endlocal
+if %all%==0 (
+echo %title% テキストに実行するSQLファイルが記載されていません。バッチの実行を中止します。 
+echo %title% テキストに実行するSQLファイルが記載されていません。バッチの実行を中止します。 >>%result%&goto :eof
+)
 
-for /f %%c in (%paths%) do (
-SET txt=%%c
-SET /a all+=1
-)
 
 setlocal enabledelayedexpansion
 for /f %%a in (%paths%) do (
+echo %title% !count!/%all% %%aの実行を始めます。 
 echo %title% !count!/%all% %%aの実行を始めます。 >>%result%
 mysql --defaults-extra-file=%cnf% <%%a >>%result% 2>&1
 if !errorlevel! equ 1 (
-ECHO %title%  !count!/%all% %%aの実行が失敗しました。>>%result%&goto :eof  
+echo %title%  !count!/%all% %%aの実行が失敗しました。
+echo %title%  !count!/%all% %%aの実行が失敗しました。>>%result%&goto :eof  
 ) 
 SET /a count+=1
 )
+echo %title% バッチの実行が正常に終了しました。
 echo %title% バッチの実行が正常に終了しました。>>%result%
 endlocal
