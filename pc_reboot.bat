9e38408a62d9126d978b8ce5c2e98ff966e9b535
sakai kohei <sakai.kohei@isid-ao.co.jp>
2021-07-04 12:09:07 +0900

    コードの修正

--- pc_reboot.bat
+++ pc_reboot.bat
@@ -8,14 +8,14 @@ echo %title% MySQL80の停止を開始します。 >>%result%
 net stop MySQL80 >>%result% 2>&1
 echo %title% エラーレベル=%errorlevel% >>%result%
 if not %errorlevel%==0 (
-echo MySQL80の停止に失敗しました。 >>%result%& goto :eof
+echo %title% MySQL80の停止に失敗しました。 >>%result%& goto :eof
 )
 echo %title% MySQL80の実行が正常に停止しました。 >>%result%
 
-echo %title% PCの再起動を開始します。 >>%result%
-shutdown.exe -r >>%result%
+echo %title% PCの再起動を開始します。 >>%result% 
+shutdown.exe -f -r -t 0 >>%result%
 echo %title% エラーレベル=%errorlevel% >>%result%
 if not %errorlevel%==0 (
-echo PCの再起動に失敗しました。 >>%result%& goto :eof
+echo %title% PCの再起動に失敗しました。 >>%result%& goto :eof
 )
 echo %title% PCの再起動が正常に実行されました。 >>%result%
