e4ded06fe8f9612c44d9d5c16e9b9f36453fa3b8
sakai kohei <sakai.kohei@isid-ao.co.jp>
2021-07-05 14:42:47 +0900

    コードの修正

--- pc_reboot.bat
+++ pc_reboot.bat
@@ -1,21 +1,34 @@
 ﻿@echo off
 CHCP 65001
 
-SET result="E:\logs\bat\pc_reboot\result.txt"
-SET title=[%date% %time% PC_REBOOT]
+SET result="C:\Users\la0760\Desktop\logs\pc_reboot\result.txt"
+SET title=[PC_REBOOT]
 
-echo %title% MySQL80の停止を開始します。 >>%result%
-net stop MySQL80 >>%result% 2>&1
-echo %title% エラーレベル=%errorlevel% >>%result%
-if not %errorlevel%==0 (
-echo %title% MySQL80の停止に失敗しました。 >>%result%& goto :eof
+echo %date% %time% %title% MySQL80の稼働を確認します。 >>%result%
+sc query MySQL80 | findstr STATE | findstr RUNNING > null
+echo %date% %time% %title% エラーレベル=%errorlevel% >>%result%
+if %errorlevel%==0 (
+echo %date% %time% %title% MySQL80が稼働していたため、停止を実行します。 >>%result%
+net stop MySQL80
+timeout /T 5
+setlocal enabledelayedexpansion
+sc query MySQL80 | findstr STATE | findstr RUNNING > null
+echo %date% %time% %title% エラーレベル=!errorlevel! >>%result%
+if !errorlevel!==0 (
+echo %date% %time% %title% MySQL80の停止に失敗しました。  >>%result%&goto :eof
+) else !errorlevel!==1 (
+echo %date% %time% %title% MySQL80の停止に成功しました。  >>%result%&goto :reboot
 )
-echo %title% MySQL80の実行が正常に停止しました。 >>%result%
+endlocal
+) else (
+echo %date% %time% %title% MySQL80は停止していました。 >>%result%
+)
+:reboot
 
-echo %title% PCの再起動を開始します。 >>%result% 
-shutdown.exe -f -r -t 0 >>%result%
-echo %title% エラーレベル=%errorlevel% >>%result%
+echo %date% %time% %title% PCの再起動を開始します。 >>%result%
+shutdown.exe -r -t 10 >>%result%
+echo %date% %time% %title% エラーレベル=%errorlevel% >>%result%
 if not %errorlevel%==0 (
-echo %title% PCの再起動に失敗しました。 >>%result%& goto :eof
+echo %date% %time% %title% PCの再起動に失敗しました。 >>%result%& goto :eof
 )
-echo %title% PCの再起動が正常に実行されました。 >>%result%
+echo %date% %time% %title% PCの再起動が正常に開始されました。 >>%result%
