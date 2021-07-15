9069b99faa34e8fb6ee0e2c4cda8631f3e88e45a
sakai kohei <sakai.kohei@isid-ao.co.jp>
2021-07-15 20:14:20 +0900

    不明

--- pc_reboot.bat
+++ pc_reboot.bat
@@ -2,20 +2,21 @@
 CHCP 65001
 
 SET result="C:\Users\horni\OneDrive\デスクトップ\office\E\logs\bat\pc_reboot\result.txt"
-SET title=[%date% %time% PC_REBOOT]
+SET title=[PC_REBOOT]
 
-echo %title% MySQL80の停止を開始します。 >>%result%
+echo %date% %time% %title% MySQL80の停止を開始します。 >>%result%
 net stop MySQL80 >>%result% 2>&1
-echo %title% エラーレベル=%errorlevel% >>%result%
+timeout /T 5
+echo %date% %time% %title% エラーレベル=%errorlevel% >>%result%
 if not %errorlevel%==0 (
-echo %title% MySQL80の停止に失敗しました。 >>%result%& goto :eof
+echo %date% %time% %title% MySQL80の停止に失敗しました。 >>%result%& goto :eof
 )
-echo %title% MySQL80の実行が正常に停止しました。 >>%result%
+echo %date% %time% %title% MySQL80の実行が正常に停止しました。 >>%result%
 
-echo %title% PCの再起動を開始します。 >>%result% 
-shutdown.exe -f -r -t 0 >>%result%
-echo %title% エラーレベル=%errorlevel% >>%result%
+echo %date% %time% %title% PCの再起動を開始します。 >>%result% 
+shutdown.exe -r -t 10 >>%result%
+echo %date% %time% %title% エラーレベル=%errorlevel% >>%result%
 if not %errorlevel%==0 (
-echo %title% PCの再起動に失敗しました。 >>%result%& goto :eof
+echo %date% %time% %title% PCの再起動に失敗しました。 >>%result%& goto :eof
 )
-echo %title% PCの再起動が正常に実行されました。 >>%result%
+echo %date% %time% %title% PCの再起動が正常に実行されました。 >>%result%
