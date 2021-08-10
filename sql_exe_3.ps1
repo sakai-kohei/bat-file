92155459f4d375a009ce9588fe7513c0c4db5d45
sakai kohei <sakai.kohei@isid-ao.co.jp>
2021-08-10 22:02:48 +0900

    コードの修正

--- sql_exe_3.ps1
+++ sql_exe_3.ps1
@@ -14,31 +14,30 @@ txtは、実行するSQLファイル名を一時的に格納する変数
 $cnf="C:\ProgramData\MySQL\MySQL Server 8.0\login.cnf"
 $paths="E:\sql_exe.txt"
 $result="E:\logs\bat\sql_exe\result.txt"
-$date=Get-Date
 $title="SQL_EXE"
 $count=0
 $txt = (Get-Content $paths -Encoding UTF8)
 
 for ($i=0; $i -lt $txt.Length; $i++) {
  if(($txt[$i] -eq "")){
-  echo "[$date $title]  テキストに実行するSQLファイルが記載されていません。バッチの実行を中止します。 " |  Add-Content $result -Encoding UTF8 –pass
+  echo ("["+(Get-Date -Format G)+" $title]  テキストに実行するSQLファイルが記載されていません。バッチの実行を中止します。 ") |  Add-Content $result -Encoding UTF8 –pass
   exit
  }
  if(-not(Test-Path $txt[$i])) {
-  echo "[$date $title] 「sql_exe.txt」に記入したSQLファイルのパスが間違っています。バッチの実行を中止します。 " |  Add-Content $result -Encoding UTF8 –pass
+  echo ("["+(Get-Date -Format G)+" $title] 「sql_exe.txt」に記入したSQLファイルのパスが間違っています。バッチの実行を中止します。 ") |  Add-Content $result -Encoding UTF8 –pass
   exit
  }
  $sql=$txt[$i].Substring($txt[$i].Length - 4, 4)
  if (-not($sql -eq ".sql" )){
-  echo "[$date $title] 「sql_exe.txt」には、SQLファイルを記入してください。バッチの実行を中止します。" |  Add-Content $result -Encoding UTF8 –pass
+  echo ("["+(Get-Date -Format G)+" $title] 「sql_exe.txt」には、SQLファイルを記入してください。バッチの実行を中止します。") |  Add-Content $result -Encoding UTF8 –pass
   exit
  }
  $count=$i+1
- echo ("[$date $title]"+" "+"$count"+"/"+ @($txt).Length+" "+@($txt)[$i]+"の実行を始めます。") |  Add-Content $result -Encoding UTF8 –pass
+ echo ("["+(Get-Date -Format G)+" $title]"+" "+"$count"+"/"+ @($txt).Length+" "+@($txt)[$i]+"の実行を始めます。") |  Add-Content $result -Encoding UTF8 –pass
  Get-Content @($txt)[$i] | mysql --defaults-extra-file="$cnf" |  Add-Content $result -Encoding UTF8 –pass 2>&1
  if($LASTEXITCODE -eq 1){
-  echo ("[$date $title]"+" "+"$count"+"/"+ @($txt).Length+" "+@($txt)[$i]+"の実行が失敗しました。") |  Add-Content $result -Encoding UTF8 –pass
+  echo ("["+(Get-Date -Format G)+" $title]"+" "+"$count"+"/"+ @($txt).Length+" "+@($txt)[$i]+"の実行が失敗しました。") |  Add-Content $result -Encoding UTF8 –pass
   exit
  }
 }
-echo "[$date $title] バッチの実行が正常に終了しました。" |  Add-Content $result -Encoding UTF8 –pass
+echo ("["+(Get-Date -Format G)+" $title] バッチの実行が正常に終了しました。") |  Add-Content $result -Encoding UTF8 –pass
