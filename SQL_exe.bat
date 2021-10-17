e20de65de1e0f0f2ea05795e6acab9586030482b
sakai-kohei <sakai.kohei@isid-ao.co.jp>
2021-10-18 08:58:41 +0900

    パスの再設定

--- SQL_exe.bat
+++ SQL_exe.bat
@@ -1,4 +1,4 @@
 ﻿@echo off
 CHCP 65001
 
-powershell -File "F:\04_bat_file\sql_exe_3.ps1"
+powershell -File "F:\04_bat_file\sql_exe.ps1"
---
+++ sql_exe.ps1
@@ -0,0 +1,44 @@
+﻿<# =====================================================================
+SQLファイル実行バッチ Ver1.0
+Date  :2021/06/28
+
+cnfは、MySQLへのログイン情報を記載しているテキスト
+pathsは、実行するSQLファイルを記載するテキスト
+resultは、実行時のメッセージを格納するテキスト
+titleは、実行メッセージのタイトル
+allは、実行したいSQLファイルの総数を格納する変数
+countは、実行するSQLファイルの項番を格納する変数
+txtは、実行するSQLファイル名を一時的に格納する変数
+=====================================================================#>
+
+$cnf="C:\ProgramData\MySQL\MySQL Server 8.0\login.cnf"
+$paths="F:\sql_exe.txt"
+$result="F:\05_logs\bat\sql_exe\result.txt"
+$title="SQL_EXE"
+$count=0
+$txt = @(Get-Content $paths -Encoding UTF8)
+
+if(($txt.Length -eq 0)){
+  echo ("["+(Get-Date -Format G)+" $title]  テキストに実行するSQLファイルが記載されていません。バッチの実行を中止します。 ") |  Add-Content $result -Encoding UTF8 –pass
+  exit
+ }
+
+for ($i=0; $i -lt $txt.Length; $i++) {
+ if(-not(Test-Path @($txt[$i]))) {
+  echo ("["+(Get-Date -Format G)+" $title] 「sql_exe.txt」に記入したSQLファイルのパスが間違っています。バッチの実行を中止します。 ") |  Add-Content $result -Encoding UTF8 –pass
+  exit
+ }
+ $sql=$txt[$i].Substring($txt[$i].Length - 4, 4)
+ if (-not($sql -eq ".sql" )){
+  echo ("["+(Get-Date -Format G)+" $title] 「sql_exe.txt」には、SQLファイルを記入してください。バッチの実行を中止します。") |  Add-Content $result -Encoding UTF8 –pass
+  exit
+ }
+ $count=$i+1
+ echo ("["+(Get-Date -Format G)+" $title]"+" "+"$count"+"/"+ @($txt).Length+" "+@($txt)[$i]+"の実行を始めます。") |  Add-Content $result -Encoding UTF8 –pass
+ Get-Content @($txt)[$i] | mysql --defaults-extra-file="$cnf" |  Add-Content $result -Encoding UTF8 –pass 2>&1
+ if($LASTEXITCODE -eq 1){
+  echo ("["+(Get-Date -Format G)+" $title]"+" "+"$count"+"/"+ @($txt).Length+" "+@($txt)[$i]+"の実行が失敗しました。") |  Add-Content $result -Encoding UTF8 –pass
+  exit
+ }
+}
+echo ("["+(Get-Date -Format G)+" $title] バッチの実行が正常に終了しました。") |  Add-Content $result -Encoding UTF8 –pass
